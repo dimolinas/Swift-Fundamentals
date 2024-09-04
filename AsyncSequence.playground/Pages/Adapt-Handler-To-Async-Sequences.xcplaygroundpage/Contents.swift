3d7e08cf5ef8ad0e157022d997ce3ec78fe5742e
Diego Molina <dimolinas@unal.edu.co>
2024-09-03 22:39:17 -0500

    other files

--- AsyncSequence.playground/Pages/Adapt-Handler-To-Async-Sequences.xcplaygroundpage/Contents.swift
+++ AsyncSequence.playground/Pages/Adapt-Handler-To-Async-Sequences.xcplaygroundpage/Contents.swift
@@ -1,7 +1,46 @@
-//: [Previous](@previous)
+import UIKit
 
-import Foundation
+class BitcoinPriceMonitor {
+    var price: Double = 0.0
+    var timer: Timer?
+    var priceHandler: (Double) -> Void = { _ in }
+    
+    @objc func getPrice(){
+        priceHandler(Double.random(in: 2000...40000))
+    }
+    
+    func startUpdating(){
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(getPrice), userInfo: nil, repeats: true)
+    }
+    
+    func stopUpdating(){
+        timer?.invalidate()
+    }
+}
 
-var greeting = "Hello, playground"
+let bitcoinPriceMonitor = BitcoinPriceMonitor()
 
-//: [Next](@next)
+bitcoinPriceMonitor.priceHandler = {
+    print("handler",$0)
+}
+
+bitcoinPriceMonitor.startUpdating()
+
+
+let bitcoinStream = AsyncStream(Double.self) { continuation in
+    let bitcoinPriceMonitor = BitcoinPriceMonitor()
+    bitcoinPriceMonitor.priceHandler = {
+        continuation.yield( $0)
+    }
+    
+    //continuation.onTermination = {_ in }
+    
+    bitcoinPriceMonitor.startUpdating()
+}
+
+
+Task {
+    for await bitcoinPrice in bitcoinStream {
+        print(bitcoinPrice)
+    }
+}
--- AsyncSequence.playground/Pages/UrlSession.xcplaygroundpage/Contents.swift
+++ AsyncSequence.playground/Pages/UrlSession.xcplaygroundpage/Contents.swift
@@ -1,7 +1,24 @@
-//: [Previous](@previous)
-
 import Foundation
+import UIKit
 
-var greeting = "Hello, playground"
+let url = URL(string: "https://www.google.com")!
 
-//: [Next](@next)
+Task{
+    print("Starting Task 1")
+    let (bytes, _) = try await URLSession.shared.bytes(from: url)
+    for try await byte in bytes {
+        print(byte)
+    }
+}
+
+
+Task{
+    print("Starting Task 2")
+    let center = NotificationCenter.default
+    await center.notifications(named: UIApplication.didEnterBackgroundNotification).first {
+        guard let key = ($0.userInfo?["Key"]) as? String else { return false }
+        return key == "Some Value"
+    }
+}
+
+
